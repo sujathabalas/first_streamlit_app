9f00dec854e6c03bc27030cf2ef4ee0958c2d12b
sujathabalas <140990480+sujathabalas@users.noreply.github.com>
2023-07-31 13:36:15 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit
-streamlit.title('My breakfast and had healthy dinner')
-
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
